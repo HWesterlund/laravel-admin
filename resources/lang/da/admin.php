86edb57707b194c5f469cffff58f9d361fffa667
Hans Westerlund <hpwesterlund@gmail.com>
2018-12-30 21:59:09 +0100

    untracked files

---
+++ resources/lang/da/admin.php
@@ -0,0 +1,112 @@
+<?php
+
+return [
+    'online'                => 'Online',
+    'login'                 => 'Log ind',
+    'logout'                => 'Log ud',
+    'setting'               => 'Indstilling',
+    'name'                  => 'Navn',
+    'username'              => 'Brugernavn',
+    'password'              => 'Adgangskode',
+    'password_confirmation' => 'Bekræft adgangskode',
+    'remember_me'           => 'Husk mig',
+    'user_setting'          => 'Bruger indstilling',
+    'avatar'                => 'Avatar',
+
+    'list'         => 'Liste',
+    'new'          => 'Ny',
+    'create'       => 'Opret',
+    'delete'       => 'Slet',
+    'remove'       => 'Fjern',
+    'edit'         => 'Rediger',
+    'view'         => 'Vis',
+    'browse'       => 'Browse',
+    'reset'        => 'Nulstil',
+    'export'       => 'Eksporter',
+    'batch_delete' => 'Slet flere',
+    'save'         => 'Gem',
+    'refresh'      => 'Refresh',
+    'order'        => 'Rækkefølge',
+    'expand'       => 'Fold ud',
+    'collapse'     => 'Fold ind',
+    'filter'       => 'Filtrer',
+    'close'        => 'Luk',
+    'show'         => 'Vis',
+    'entries'      => 'emner',
+    'captcha'      => 'Captcha',
+
+    'action'            => 'Handling',
+    'title'             => 'Titel',
+    'description'       => 'Beskrivelse',
+    'back'              => 'Tilbage',
+    'back_to_list'      => 'Tilbage til listen',
+    'submit'            => 'Send',
+    'menu'              => 'Menu',
+    'input'             => 'Indtast',
+    'succeeded'         => 'Succesfuld',
+    'failed'            => 'Fejlede',
+    'delete_confirm'    => 'Er du sikker ?',
+    'delete_succeeded'  => 'Slettet',
+    'delete_failed'     => 'Kunne ikke slettes !',
+    'update_succeeded'  => 'Opdaterert !',
+    'save_succeeded'    => 'Gemt !',
+    'refresh_succeeded' => 'Genindlæst !',
+    'login_successful'  => 'Logget ind',
+
+    'choose'       => 'Vælg',
+    'choose_file'  => 'Vælg fil',
+    'choose_image' => 'Vælg billede',
+
+    'more' => 'Mere',
+    'deny' => 'Adgang nægtet',
+
+    'administrator' => 'Administrator',
+    'roles'         => 'Roller',
+    'permissions'   => 'Rettigheder',
+    'slug'          => 'Slug',
+
+    'created_at' => 'Oprettet',
+    'updated_at' => 'Opdateret',
+
+    'alert' => 'Alert',
+
+    'parent_id' => 'Parent',
+    'icon'      => 'Icon',
+    'uri'       => 'URI',
+
+    'operation_log'       => 'Handlings log',
+    'parent_select_error' => 'Parent select error',
+
+    'pagination' => [
+        'range' => 'Viser :first til :last af :total resultater',
+    ],
+
+    'role'       => 'Rolle',
+    'permission' => 'Rettighed',
+    'route'      => 'Route',
+    'confirm'    => 'Bekræft',
+    'cancel'     => 'Annuler',
+
+    'http' => [
+        'method' => 'HTTP method',
+        'path'   => 'HTTP path',
+    ],
+    'all_methods_if_empty' => 'All methods if empty',
+
+    'all'           => 'Alle',
+    'current_page'  => 'Nuværende side',
+    'selected_rows' => 'Valgte rækker',
+
+    'upload'     => 'Upload',
+    'new_folder' => 'Ny mappe',
+    'time'       => 'Tid',
+    'size'       => 'Størrelse',
+
+    'listbox' => [
+        'text_total'         => 'Viser alle {0}',
+        'text_empty'         => 'Ingen resultater',
+        'filtered'           => '{0} / {1}',
+        'filter_clear'       => 'Vis alle',
+        'filter_placeholder' => 'Filter',
+    ],
+];
